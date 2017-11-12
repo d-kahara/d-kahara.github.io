78ac52fe993d74a61da100fb7cde4c542f4a5578
David Kahara <kaharadave@gmail.com>
2017-11-12 23:38:16 +0300

    Styling projects Section

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function(){
 	$(".typed").typed({
-		strings: ["Hey !!", "I am a Web Developer.", "I am an android Developer."],
+		strings: ["Hey !!","Welcome to my personal website", "I am a Web Developer.", "I am an android Developer."],
 		// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
 		stringsElement: null,
 		// typing speed
